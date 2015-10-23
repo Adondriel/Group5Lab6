837d073e8e6d94f3a63d149dd11c77a09f63a8f2
Benjamin Uleau <benuleau@gmail.com>
2015-10-23 14:19:14 -0400

    Add the Attack UI class

--- src/ui/command/Attack.java
+++ src/ui/command/Attack.java
@@ -2,20 +2,70 @@ package ui.command;
 
 import Exceptions.EnvironmentException;
 import lifeform.LifeForm;
-
+import environment.Environment;
 /**
  * 
- * @author Bradley Solorzano
+ * @author Bradley Solorzano, Benjamin Uleau
  *	Design Pattners Group 5 lab 6
  */
 public class Attack implements Command
 {
 	private LifeForm entity;
 	private LifeForm target;
-	public void attackCommand(LifeForm L, LifeForm T)
+	
+	Environment e = Environment.getWorldInstance(5, 5);
+	private char north='n';
+	private char south='s';
+	private char east='e';
+	private char west='w';
+	public void attackCommand(int row, int col) throws EnvironmentException
 	{
-		entity = L;
-		target = T;
+		int attacked=0;
+		entity=e.getLifeForm(row, col);
+		
+		//Attack north
+		if(entity.getDirection()==north){
+			for(int i=row; i>=0; i--){
+				if(e.getLifeForm(i, col)!=null && attacked==0){
+					LifeForm lf=e.getLifeForm(i, col);
+					entity.attackLF(lf);
+					attacked++;
+				}
+			}
+		}
+		
+		//Attack south
+		if(entity.getDirection()==south){
+			for(int i=row; i<e.getMaxRow(); i++){
+				if(e.getLifeForm(i, col)!=null && attacked==0){
+					LifeForm lf=e.getLifeForm(i, col);
+					entity.attackLF(lf);
+					attacked++;
+				}
+			}
+		}
+		
+		//Attack east
+		if(entity.getDirection()==east){
+			for(int i=col; i<e.getMaxCol(); i++){
+				if(e.getLifeForm(row, i)!=null && attacked==0){
+					LifeForm lf=e.getLifeForm(row, i);
+					entity.attackLF(lf);
+					attacked++;
+				}
+			}
+		}
+		
+		//Attack west
+		if(entity.getDirection()==west){
+			for(int i=col; i>=0; i--){
+				if(e.getLifeForm(row, i)!=null && attacked==0){
+					LifeForm lf=e.getLifeForm(row, i);
+					entity.attackLF(lf);
+					attacked++;
+				}
+			}
+		}
 	}
 	@Override
 	public void execute()
