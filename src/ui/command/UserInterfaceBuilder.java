a1605b55842f220af2cedadd896e69837e932360
Jason LoBianco <jl9857@ship.edu>
2015-10-22 20:06:05 -0400

    Added tests to TestUserInterface, updated the UserInterface and UserInterfaceBuilder.

--- src/ui/command/UserInterfaceBuilder.java
+++ src/ui/command/UserInterfaceBuilder.java
@@ -1,30 +1,199 @@
 /**
  * Author: Jason LoBianco
- * Provides functionality to the user interface.
+ * Creates the UserInterface with all of the buttons.
  */
 package ui.command;
 
 import environment.Environment;
-import lifeform.LifeForm;
 
-public class UserInterfaceBuilder 
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+@SuppressWarnings("serial")
+public class UserInterfaceBuilder extends JPanel
 {
-	private Command Commands[] = new Command[9];
+	Environment e = Environment.getWorldInstance();
+	UserInterface ui = new UserInterface();
 	
-	public void CommandArray()
+	protected JButton reload, north, south, east, west, move, attack, drop, acquire;
+	static String message = "Welcome";
+	
+	/**
+	 * Creates the Frame and Panel that all of the buttons go into.
+	 */
+	public UserInterfaceBuilder()
 	{
-		Commands[0] = new Reload();
-		Commands[1] = new North();
-		Commands[2] = new South();
-		Commands[3] = new East();
-		Commands[4] = new West();
-		Commands[5] = new Move();
-		Commands[6] = new Attack();
-		Commands[7] = new Drop();
-		Commands[8] = new Aquire();
+		JFrame frame = new JFrame("Commands");
+		frame.setVisible(true);
+		frame.setSize(800, 250);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel buttonPanel = new JPanel(new GridBagLayout());
+		frame.getContentPane().add(buttonPanel, BorderLayout.WEST);
+		GridBagConstraints c = new GridBagConstraints();
 		
-	}
-	
-	
+		ui.addCommand(0, new Reload());
+		ui.addCommand(1, new North());
+		ui.addCommand(2, new South());
+		ui.addCommand(3, new East());
+		ui.addCommand(4, new West());
+		ui.addCommand(5, new Move());
+		ui.addCommand(6, new Attack());
+		ui.addCommand(7, new Drop());
+		ui.addCommand(8, new Aquire());
+		
+		/**
+		 * Creates the reload button with executeCommand.
+		 */
+		reload = new JButton("Reload");
+		c.gridx = 0;
+		c.gridy = 2;
+		c.insets = new Insets(10,10,10,10);
+		buttonPanel.add(reload, c);
+		reload.addActionListener(new ActionListener(){
+		@Override
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			ui.executeCommand(0);
+		}
+		});
+		
+		/**
+		 * Creates the North button with executeCommand.
+		 */
+		JButton north = new JButton("North");
+		c.gridx = 4;
+		c.gridy = 0;
+		buttonPanel.add(north, c);
+		north.addActionListener(new ActionListener(){
+		@Override
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			ui.executeCommand(1);
+		}
+		});
+		
+		/**
+		 * Creates the South button with executeCommand.
+		 */
+		JButton south = new JButton("South");
+		c.gridx = 4;
+		c.gridy = 2;
+		buttonPanel.add(south, c);
+		south.addActionListener(new ActionListener(){
+		@Override
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			ui.executeCommand(2);
+		}
+		});
+		
+		/**
+		 * Creates the East button with executeCommand.
+		 */
+		JButton east = new JButton("East");
+		c.gridx = 5;
+		c.gridy = 1;
+		buttonPanel.add(east, c);
+		east.addActionListener(new ActionListener(){
+		@Override
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			ui.executeCommand(3);
+		}
+		});
+		
+		/**
+		 * Creates the West button with executeCommand.
+		 */
+		JButton west = new JButton("West");
+		c.gridx = 3;
+		c.gridy = 1;
+		buttonPanel.add(west, c);
+		west.addActionListener(new ActionListener(){
+		@Override
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			ui.executeCommand(4);
+		}
+		});
+		
+		/**
+		 * Creates the Move button with executeCommand.
+		 */
+		JButton move = new JButton("M");
+		c.gridx = 4;
+		c.gridy = 1;
+		buttonPanel.add(move, c);
+		move.addActionListener(new ActionListener(){
+		@Override
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			ui.executeCommand(5);
+		}
+		});
+		
+		/**
+		 * Creates the Attack button with executeCommand.
+		 */
+		JButton attack = new JButton("Attack");
+		c.gridx = 0;
+		c.gridy = 0;
+		buttonPanel.add(attack, c);
+		attack.addActionListener(new ActionListener(){
+		@Override
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			ui.executeCommand(6);
+		}
+		});
+		
+		/**
+		 * Creates the Drop button with executeCommand.
+		 */
+		JButton drop = new JButton("Drop");
+		c.gridx = 1;
+		c.gridy = 2;
+		buttonPanel.add(drop, c);
+		drop.addActionListener(new ActionListener(){
+		@Override
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			ui.executeCommand(7);
+		}
+		});
+		
+		/**
+		 * Creates the Acquire button with executeCommand.
+		 */
+		JButton acquire = new JButton("Acquire");
+		c.gridx = 1;
+		c.gridy = 0;
+		buttonPanel.add(acquire, c);
+		acquire.addActionListener(new ActionListener(){
+		@Override
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			ui.executeCommand(8);
+		}
+		});
+		
+		/**
+		 * Creates the Prompt message.
+		 */
+		JLabel prompt = new JLabel(message);
+		c.gridx = 6;
+		c.gridy = 0;
+		buttonPanel.add(prompt, c);
+		
+		/**
+		 * Creates the UserInput box.
+		 */
+		JTextField userinput = new JTextField(10);
+		c.gridx = 6;
+		c.gridy = 2;
+		buttonPanel.add(userinput, c);
+		
+	}	
 }
-	
--- test/ui/command/TestUserInterface.java
+++ test/ui/command/TestUserInterface.java
@@ -7,10 +7,23 @@ package ui.command;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+import weapon.ChainGun;
+import Exceptions.RecovRateIsNegative;
+import environment.Environment;
+
 import javax.swing.*;
+
+import lifeform.Human;
+import lifeform.LifeForm;
+import lifeform.MockLifeForm;
 
 public class TestUserInterface 
 {
+	private char north='n';
+	private char south='s';
+	private char east='e';
+	private char west='w';
 
 	/**
 	 * Checks to see that the UserInterface is initialized correctly.
@@ -18,7 +31,7 @@ public class TestUserInterface
 	@Test
 	public void testInitialization() 
 	{
-		UserInterface ui = new UserInterface();
+		UserInterfaceBuilder ui = new UserInterfaceBuilder();
 		assertEquals(JOptionPane.YES_OPTION, JOptionPane.showConfirmDialog(null, "Did a window open up?"));
 		assertEquals(JOptionPane.YES_OPTION, JOptionPane.showConfirmDialog(null, "Is there an attack button on the top left?"));
 		assertEquals(JOptionPane.YES_OPTION, JOptionPane.showConfirmDialog(null, "Is there a reload button on the bottom left?"));
@@ -30,5 +43,126 @@ public class TestUserInterface
 		assertEquals(JOptionPane.YES_OPTION, JOptionPane.showConfirmDialog(null, "Is there a West button?"));
 		assertEquals(JOptionPane.YES_OPTION, JOptionPane.showConfirmDialog(null, "Is there a East button?"));
 	}
-
+	
+	/**
+	 * Checks to see that clicking the turn north button turns the LifeForm north.
+	 * @throws RecovRateIsNegative 
+	 * @throws InterruptedException 
+	 */
+	@Test
+	public void testTurnNorth() throws RecovRateIsNegative, InterruptedException
+	{
+		UserInterfaceBuilder ui = new UserInterfaceBuilder();
+		Human h = new Human("Bob", 30, 15);
+		JOptionPane.showInputDialog("Click the north button.");
+		Thread.sleep(7500);
+		assertEquals(north, h.getDirection());
+	}
+	
+	/**
+	 * Checks to see that clicking the turn east button turns the LifeForm east.
+	 * @throws InterruptedException 
+	 */
+	@Test
+	public void testTurnEast() throws InterruptedException
+	{
+		UserInterfaceBuilder ui = new UserInterfaceBuilder();
+		Human h = new Human("Bob", 30, 15);
+		JOptionPane.showInputDialog("Click the east button.");
+		Thread.sleep(7500);
+		assertEquals(east, h.getDirection());
+	}
+	
+	/**
+	 * Checks to see that clicking the turn south button turns the LifeForm south.
+	 * @throws InterruptedException 
+	 */
+	@Test
+	public void testTurnSouth() throws InterruptedException
+	{
+		UserInterfaceBuilder ui = new UserInterfaceBuilder();
+		Human h = new Human("Bob", 30, 15);
+		JOptionPane.showInputDialog("Click the south button.");
+		Thread.sleep(7500);
+		assertEquals(south, h.getDirection());
+	}
+	
+	/**
+	 * Checks to see that clicking the turn west button turns the LifeForm west.
+	 * @throws InterruptedException 
+	 */
+	@Test
+	public void testTurnWest() throws InterruptedException
+	{
+		UserInterfaceBuilder ui = new UserInterfaceBuilder();
+		Human h = new Human("Bob", 30, 15);
+		JOptionPane.showInputDialog("Click the west button.");
+		Thread.sleep(7500);
+		assertEquals(west, h.getDirection());
+	}
+	
+//	/**
+//	 * Checks to see that clicking the acquire button causes LifeForm to pickup a weapon.
+//	 */
+//	@Test
+//	public void testAcquire()
+//	{
+//		UserInterfaceBuilder ui = new UserInterfaceBuilder();
+//		LifeForm entity = new MockLifeForm("Bob", 40);
+//		ChainGun cg = new ChainGun();
+//		//Click the acquire button to pick up a weapon.
+//		assertEquals(cg, entity.getWeapon());
+//	}
+//	
+//	/**
+//	 * Checks to see that clicking the drop button causes LifeForm to drop a weapon.
+//	 */
+//	@Test
+//	public void testDrop()
+//	{
+//		UserInterfaceBuilder ui = new UserInterfaceBuilder();
+//		LifeForm entity = new MockLifeForm("Bob", 40);
+//		ChainGun cg = new ChainGun();
+//		//Click the acquire button to pick up a weapon.
+//		assertEquals(cg, entity.getWeapon());
+//		//Click the drop button to drop a weapon.
+//		assertEquals(null, entity.getWeapon());
+//	}
+//	
+//	/**
+//	 * Checks to see that clicking the move button causes LifeForm to move.
+//	 */
+//	@Test
+//	public void testMove()
+//	{
+//		UserInterfaceBuilder ui = new UserInterfaceBuilder();
+//		LifeForm entity = new MockLifeForm("Bob", 40);
+//	}
+//	
+//	/**
+//	 * Checks to see that clicking the attack button causes LifeForm to attack another LifeForm.
+//	 */
+//	@Test
+//	public void testAttack()
+//	{
+//		UserInterfaceBuilder ui = new UserInterfaceBuilder();
+//		LifeForm entity = new MockLifeForm("Bob", 40);
+//		LifeForm entity2 = new MockLifeForm("Fred", 30);
+//	}
+//	
+//	/**
+//	 * Checks to see that clicking the reload button causes LifeForm to reload a weapon.
+//	 */
+//	@Test
+//	public void testReload()
+//	{
+//		UserInterfaceBuilder ui = new UserInterfaceBuilder();
+//		LifeForm entity = new MockLifeForm("Bob", 40);
+//		ChainGun cg = new ChainGun();
+//		//Click the acquire button to pick up a weapon.
+//		assertEquals(cg, entity.getWeapon());
+//		cg.setCurrentAmmo(10);
+//		//Click the reload button to reload the weapon.
+//		assertEquals(40, cg.getCurrentAmmo());
+//	}	
 }
